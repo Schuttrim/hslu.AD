a7177a89155b151c182c2e287ec8b60d32294647
Lukas Eck <lukas.eck@stud.hslu.ch>
2020-02-25 14:11:34 +0100

    allocation hashcode and equality implemented

--- AD Exercises/src/main/java/ch/hslu/memory/Allocation.java
+++ AD Exercises/src/main/java/ch/hslu/memory/Allocation.java
@@ -1,6 +1,36 @@
 package ch.hslu.memory;
 
-public class Allocation {
-    private int startAdress;
-    private int size;
+import java.lang.reflect.Type;
+
+public final class Allocation {
+    private final int startAdress;
+
+    public int getStartAdress() {
+        return startAdress;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private final int size;
+
+    public Allocation(int startAdress, int size){
+        this.startAdress = startAdress;
+        this.size = size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode (this.startAdress + this.size + 31);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Allocation)){
+            return false;
+        }
+        Allocation alloc = (Allocation) obj;
+        return this.size == alloc.size && this.startAdress == alloc.size;
+    }
 }
