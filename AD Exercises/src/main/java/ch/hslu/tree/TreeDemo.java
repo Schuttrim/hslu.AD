b635c3b3b08de47ea624cbd101e40b72af636684
Lukas Eck <lukas.eck@stud.hslu.ch>
2020-03-08 19:06:09 +0100

    allocation demo added

--- AD Exercises/src/main/java/ch/hslu/tree/TreeDemo.java
+++ AD Exercises/src/main/java/ch/hslu/tree/TreeDemo.java
@@ -1,5 +1,6 @@
 package ch.hslu.tree;
 
+import ch.hslu.memory.Allocation;
 import com.sun.source.util.Trees;
 
 import java.util.Iterator;
@@ -8,7 +9,29 @@ public class TreeDemo {
     public static void main(final String[] args){
         //IntegerSetDemo();
 
+        AllocationDemo();
+    }
 
+    private static void AllocationDemo() {
+        Tree<Allocation> allocationTree = TreeSet.Create(((o1, o2) -> o1.getSize() - o2.getSize()));
+
+        Allocation alloc1 = new Allocation(0, 15);
+        Allocation alloc2 = new Allocation(0, 4);
+        Allocation alloc3 = new Allocation(0, 100);
+        Allocation alloc4 = new Allocation(0, 11);
+        Allocation alloc5 = new Allocation(0, 23);
+
+        allocationTree.add(alloc1);
+        allocationTree.add(alloc2);
+        allocationTree.add(alloc3);
+        allocationTree.add(alloc4);
+        allocationTree.add(alloc5);
+
+        Iterator<Allocation> iterator = allocationTree.inorder();
+        while (iterator.hasNext()){
+            Allocation alloc = iterator.next();
+            System.out.println(alloc.getSize());
+        }
     }
 
     private static void IntegerSetDemo() {
