0ba4b71bf6519eff52cd31ea679a032d15978b92
Lukas Eck <lukas.eck@stud.hslu.ch>
2020-03-08 12:40:52 +0100

    TreeSet - add implemented

--- AD Exercises/src/main/java/ch/hslu/tree/TreeSet.java
+++ AD Exercises/src/main/java/ch/hslu/tree/TreeSet.java
@@ -8,7 +8,35 @@ import java.util.Iterator;
 public class TreeSet<T> implements Tree<T> {
     @Override
     public void add(T item) {
-        throw new Error("Not Implemented");
+        if (this.root == null){
+            this.root = new Node<T>(item);
+            return;
+        }
+
+        Node<T> result = this.getParentOrNode(this.root, item);
+        int compare = this.compareInOrder(item, root.getValue());
+        if (compare < 0) {
+            result.setLeftNode(new Node<T>(item));
+        } else if (compare > 0){
+            result.setRightNode(new Node<T>(item));
+        }
+    }
+
+    private int compareInOrder(T value, T nodeValue){
+        return this.comparator.compare(value, nodeValue);
+    }
+
+    private Node<T> getParentOrNode(Node<T> current, T value){
+        if (current.isLeaf())
+            return current;
+        int result = this.compareInOrder(value, current.getValue());
+        if (result < 0){
+            return this.getParentOrNode(current.getLeftNode(), value);
+        } else if (result > 0) {
+            return this.getParentOrNode(current.getRightNode(), value);
+        } else {
+            return current;
+        }
     }
 
     @Override
@@ -27,6 +55,7 @@ public class TreeSet<T> implements Tree<T> {
     }
 
     private Comparator<T> comparator;
+    private Node<T> root;
 
     private TreeSet(Comparator<T> comparator){
         this.comparator = comparator;
