1c2d4a88a80d2bbe3c564db531ef0066b9ade785
Lukas Eck <lukas.eck@stud.hslu.ch>
2020-03-17 17:47:31 +0100

    Tests added

--- AD Exercises/src/test/java/ch/hslu/hashtable/HashTableImplTest.java
+++ AD Exercises/src/test/java/ch/hslu/hashtable/HashTableImplTest.java
@@ -6,12 +6,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HashTableImplTest {
     @Test
-    void AddTest() {
+    void AddAndHasTest() {
         HashTable<FancyInteger> table = new HashTableImpl<>();
         assertFalse(table.has(new FancyInteger(5)));
 
         table.add(new FancyInteger(5));
 
         assertTrue(table.has(new FancyInteger(5)));
+    }
+
+    @Test
+    void SearchTestDoesntContain() {
+        HashTable<FancyInteger> table = new HashTableImpl<>();
+        table.add(new FancyInteger(3));
+
+        assertNull(table.search(new FancyInteger(5)));
+    }
+
+    @Test
+    void SearchTestContains() {
+        HashTable<FancyInteger> table = new HashTableImpl<>();
+        table.add(new FancyInteger(3));
+
+        assertNotNull(table.search(new FancyInteger(3)));
+    }
+
+    @Test
+    void RemoveTest() {
+        HashTable<FancyInteger> table = new HashTableImpl<>();
+        assertFalse(table.has(new FancyInteger(5)));
+        table.add(new FancyInteger(5));
+        assertTrue(table.has(new FancyInteger(5)));
+
+        table.remove(new FancyInteger(5));
+
+        assertFalse(table.has(new FancyInteger(5)));
     }
 }
